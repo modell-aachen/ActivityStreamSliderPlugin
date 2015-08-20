e8514806926de425bddb4ebb948d716870fe4be0
thulfaut <thulfaut@modell-aachen.de>
2015-08-20 13:53:53 +0200

    added node_modules etc to git

---
+++ pub/System/ActivityStreamSliderPlugin/build/src/app/services/aggregated_service.js
@@ -0,0 +1,65 @@
+(function() {
+    angular
+        .module('ngActivityStreamSlider')
+        .factory('aggregatedService', aggregatedService);
+
+
+    function aggregatedService($http, $q, aggregatedModelMockService) {
+        var service = {
+            get_updates: get_updates,
+            toggle_status: toggle_status,
+            flush: flush,
+            get_total_unread: get_total_unread
+        };
+
+        ////////////
+        
+        var items = [];
+
+        var offset = 0;
+
+        var num = 2;
+
+        function get_updates() {
+            var deferred = $q.defer();
+            
+            var new_items = aggregatedModelMockService.fetch_items(num,offset);
+            new_items.then(function(result) {
+                        items = items.concat(result.items);
+                        offset += result.num_items;
+                        deferred.resolve(items);
+            }, function(error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        }
+
+        function toggle_status(item) {
+            if(item.read){
+                return aggregatedModelMockService.set_unread(item.id);
+            } else {
+                return aggregatedModelMockService.set_read(item.id);
+            }
+        }
+
+        function flush(){
+            items = [];
+            offset = 0;
+        }
+
+        function get_total_unread(){
+            return aggregatedModelMockService.get_total_unread();
+            
+        }
+
+
+
+        ////////////
+        return service;
+
+    }
+    aggregatedService.$inject = ["$http", "$q", "aggregatedModelMockService"];
+
+
+})();
---
+++ pub/System/ActivityStreamSliderPlugin/build/src/app/services/single_service.js
@@ -0,0 +1,59 @@
+(function() {
+    angular
+        .module('ngActivityStreamSlider')
+        .factory('singleService', singleService);
+
+
+    function singleService($http, $q, singleModelMockService) {
+        var service = {
+            get_updates: get_updates,
+            toggle_status: toggle_status,
+            flush: flush
+        };
+
+        ////////////
+        
+        var items = [];
+
+        var offset = 0;
+
+        var num = 2;
+
+        function get_updates() {
+            var deferred = $q.defer();
+            
+            var new_items = singleModelMockService.fetch_items(num,offset);
+            new_items.then(function(result) {
+                        items = items.concat(result.items);
+                        offset += result.num_items;
+                        deferred.resolve(items);
+            }, function(error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        }
+
+        function toggle_status(item) {
+            if(item.read){
+                return singleModelMockService.set_unread(item.id);
+            } else {
+                return singleModelMockService.set_read(item.id);
+            }
+        }
+
+          function flush(){
+            items = [];
+            offset = 0;
+        }
+
+
+
+        ////////////
+        return service;
+
+    }
+    singleService.$inject = ["$http", "$q", "singleModelMockService"];
+
+
+})();
