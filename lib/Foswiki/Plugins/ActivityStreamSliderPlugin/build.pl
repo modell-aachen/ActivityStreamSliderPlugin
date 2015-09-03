c05332993599667aaf6b6a6708747336394179dd
Stephan Osthold <osthold@modell-aachen.de>
2015-09-03 09:42:22 +0200

    GZipping js/css files

--- lib/Foswiki/Plugins/ActivityStreamSliderPlugin/build.pl
+++ lib/Foswiki/Plugins/ActivityStreamSliderPlugin/build.pl
@@ -26,6 +26,13 @@ sub target_compress {
   print "Uglifying...\n";
   print $this->sys_action( qw(grunt uglify) );
   print "\nDone!\n\n";
+
+  print "GZipping...\n";
+  foreach my $file ( @{ $this->{files} } ) {
+    next unless $file->{name} =~ m/(?:css|js)\.gz$/;
+    print "error compressing $file->{name}\n" unless $this->_build_gz( $this->{basedir} . '/' . $file->{name} );
+  }
+  print "\nDone!\n\n";
 }
 
 sub _installDeps {
@@ -39,6 +46,61 @@ sub _installDeps {
   print $this->sys_action( qw(npm install) );
   print "\nDone!\n\n";
 
+}
+
+# XXX Copy/Paste BuildContrib/lib/Foswiki/Contrib/BuildContrib/Targets/compress.pm
+sub _needsBuilding {
+    my ( $from, $to ) = @_;
+
+    if ( -e $to ) {
+        my @fstat = stat($from);
+        my @tstat = stat($to);
+        return 0 if ( $tstat[9] >= $fstat[9] );
+    }
+    return 1;
+}
+
+# XXX Copy/Paste BuildContrib/lib/Foswiki/Contrib/BuildContrib/Targets/compress.pm
+# Uses Compress::Zlib to gzip files
+#
+#   * xxx.yyy -> xxx.yyy.gz
+#
+
+sub _build_gz {
+    my ( $this, $to ) = @_;
+
+    unless ( eval { require Compress::Zlib } ) {
+        warn "Cannot gzip: $@\n";
+        return 0;
+    }
+
+    my $from = $to;
+    $from =~ s/\.gz$// or return 0;
+    return 0 unless -e $from && _needsBuilding( $from, $to );
+
+    if ( -l $to ) {
+
+        # BuildContrib will always override links created by pseudo-install
+        unlink($to);
+    }
+
+    my $f;
+    open( $f, '<', $from ) || die $!;
+    local $/ = undef;
+    my $text = <$f>;
+    close($f);
+
+    $text = Compress::Zlib::memGzip($text);
+
+    unless ( $this->{-n} ) {
+        my $f;
+        open( $f, '>', $to ) || die "$to: $!";
+        binmode $f;
+        print $f $text;
+        close($f);
+        warn "Generated $to from $from\n";
+    }
+    return 1;
 }
 
 package main;
